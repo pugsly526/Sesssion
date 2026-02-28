31f2918ea13d3309da9a270e1fc58dfe3b8d1326
pugsly526 <pugsly526@gmail.com>
2026-02-28 20:47:50 +0300

    Refactor routes and improve file serving

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
+
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/xbt', (req, res) => {
+  res.sendFile(path.join(__dirname, 'xbt.html'));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star PEAKY-BLINDER-MD 
+  console.log(`
+Deployment Successful!
 
- Server running on http://localhost:` + PORT)
-})
+nebula-assassin-Server Running on http://localhost:` + PORT)
+});
 
-module.exports = app
+module.exports = app;
