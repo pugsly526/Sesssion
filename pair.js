4985a441db5d095d7a4550a6d16eead9e12945d1
altzzdevs <altzzdevs@gmail.com>
2026-03-28 13:07:32 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "blinder~" + string_session;
+                        let md = "cyberia~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*Hey there, PEAKY-BLINDER-MD User!* 👋🏻
 
