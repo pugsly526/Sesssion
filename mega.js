54aabd640e49803a5662b55800208ba09734e83d
pugsly526 <pugsly526@gmail.com>
2026-08-02 11:25:50 +0300

    Refactor mega.js for async upload and ES6 imports
    Refactor upload function to use async/await and modern import syntax.

--- mega.js
+++ mega.js
@@ -1,43 +1,20 @@
-const mega = require("megajs");
+import { Storage } from 'megajs';
+
 const auth = {
-  email: "beamer9146@gmail.com",
-  password: "sanglee9146",
-  userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+    email: process.env.MEGA_EMAIL || 'itz.3.kid@gmail.com',
+    password: process.env.MEGA_PASSWORD || 'Clare2008@2008',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
-const upload = (data, name) => {
-  return new Promise((resolve, reject) => {
-    const storage = new mega.Storage(auth);
+export const upload = async (data, name) => {
+    if (typeof data === 'string') data = Buffer.from(data);
 
-    // Wait for storage to be ready
-    storage.on("ready", () => {
-      console.log("Storage is ready. Proceeding with upload.");
-
-      const uploadStream = storage.upload({ name, allowUploadBuffering: true });
-
-      uploadStream.on("complete", (file) => {
-        file.link((err, url) => {
-          if (err) {
-            reject(err);
-          } else {
-            storage.close();
-            resolve(url);
-          }
-        });
-      });
-
-      uploadStream.on("error", (err) => {
-        reject(err);
-      });
-
-      data.pipe(uploadStream);
-    });
-
-    storage.on("error", (err) => {
-      reject(err);
-    });
-  });
+    const storage = await new Storage({ ...auth }).ready;
+    try {
+        const file = await storage.upload({ name, size: data.length }, data).complete;
+        const url = await file.link();
+        return url;
+    } finally {
+        storage.close();
+    }
 };
-
-module.exports = { upload };
