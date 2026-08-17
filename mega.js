6a189b9143681cbd3c52c5bf721be9f5c2056488
pugsly526 <pugsly526@gmail.com>
2026-08-17 23:10:20 +0300

    Update MEGA email for authentication

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 import { Storage } from 'megajs';
 
 const auth = {
-    email: process.env.MEGA_EMAIL || 'beamer9146@gmail.com',
+    email: process.env.MEGA_EMAIL || 'johndoelee01@gmail.com',
     password: process.env.MEGA_PASSWORD || 'sanglee9146118',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
